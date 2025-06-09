393604554c5a80e674501f58babfd7853b70b65c
timoursfoil <thewesleyhorton@gmail.com>
2025-06-09 09:59:48 -0400

    Added better hitbox detection
    Added hitbox detection better, made it so that it wont mess up physics, and makes it place the hitbox infront of your character better.

--- AbilitiesModule.lua
+++ AbilitiesModule.lua
@@ -8,12 +8,12 @@ local debounce = {} -- Debounce to prevent players from spamming abilities
 local abilities = { -- Table of the abilities
 	Kick = { -- Basic ability as an example
 		Damage = 5, -- How much damage it will do to humanoids around it
-		Hitbox = Vector3.new(5.479, 10.182, 3.647), -- Hitbox of the ability (the range that it will hit enemies from)
+		Hitbox = Vector3.new(7.479, 10.182, 5.647), -- Hitbox of the ability (the range that it will hit enemies from)
 		Animation = "rbxassetid://81035389795198", -- Animation to play to show ability
 		Cooldown = 2 -- Cooldown before you can use the next ability
 	},
 	-- You can add more using that format
-	
+
 }
 
 function abilities.FindAbility(abilityName: string) -- Check to find if the ability exists
@@ -44,25 +44,32 @@ function abilities.KickAbility(player: Player) -- Function to Kick
 	anim.AnimationId = ability.Animation
 	local track = animator:LoadAnimation(anim) -- Loading the animation to play
 	track:Play() -- Playing the animation
-
 	
+	local CF = char:WaitForChild("HumanoidRootPart").CFrame -- CFrame of the part to use for the hitbox
+
+
+
 	local hitbox = Instance.new("Part") -- Creating the hitbox
 	hitbox.Size = abilities.Kick.Hitbox -- Setting the size of the hitbox to the ability's hitbox
 	hitbox.Transparency = 1 -- Invisible
 	hitbox.Anchored = true -- Stays in place
 	hitbox.CanCollide = false -- Cannot be collided with
+	hitbox.Massless = true -- It wont mess up physics
+	hitbox.CFrame = CF + Vector3.new(0,0,-1) -- Places it infront of your character so it can hit, the Vector is offset.
 	hitbox.Parent = workspace -- Put in workspace
+
 	task.delay(0.05, function()
 		hitbox:Destroy() -- Destroys the hitbox after 0.05 seconds
 	end)
 
 	local touched = {} -- Table to make sure that they dont get damaged multiple times
-	
-	for i, thing in pairs(hitbox:GetTouchingParts()) do
-		if thing.Parent:FindFirstChild("Humanoid") and not touched[thing.Parent] then
+
+	for _, thing in pairs(workspace:GetPartsInPart(hitbox)) do
+		if thing.Parent:FindFirstChild("Humanoid") and not touched[thing.Parent] and thing.Parent ~= char then
 			local humanoid = thing.Parent:FindFirstChild("Humanoid")
 			humanoid:TakeDamage(ability.Damage)
 			table.insert(touched, thing.Parent)
+			print(thing.Parent)
 			-- Add whatever you need here to happen to the character
 		end
 	end
@@ -70,9 +77,9 @@ function abilities.KickAbility(player: Player) -- Function to Kick
 
 	-- Add whatever you need after this to happen before the ability is over (effects, other animations, etc)
 
-	
+
 	task.delay(ability.Cooldown, function()
-	debounce[player] = false -- To let you use another ability
+		debounce[player] = false -- To let you use another ability
 	end)
 end
 
